613f23997365557b2e7bc4cc1e791f378c42a1f0
Matthew Seiwert <matthewseiwert@gmail.com>
2025-04-20 10:03:15 -0300

    fifith commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const {createClient} = require('@supabase/supabase-js');
 
 
 const app = express();
-const post = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
